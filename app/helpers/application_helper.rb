2b3823229d013c64fb87072b8daef5cab27bd4e8
Chris Maxwell <cmaxwell@ojala.com>
2015-04-02 14:22:35 -0700

    Add starter HTML foundation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def head
+    render 'layouts/head'
+  end
+
+  def scripts
+    render 'layouts/scripts'
+  end
+
+  def browsehappy
+    render 'layouts/browsehappy'
+  end
 end
