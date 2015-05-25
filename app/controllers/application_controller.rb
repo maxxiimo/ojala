39e4845a7b0a3a0791c6727937d03b571d701022
Chris Maxwell <cmaxwell@ojala.com>
2015-05-25 12:34:12 -0700

    Fix variants and layouts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,20 +7,69 @@ class ApplicationController < ActionController::Base
 
 
 
-  private
-    def detect_device_variant
-      request.variant = :tablet if browser.tablet?
-      request.variant = :desktop if !browser.mobile? && !browser.tablet?
+private
+  def detect_device_variant
+    request.variant = :tablet if browser.tablet?
+    request.variant = :desktop if !browser.mobile? && !browser.tablet?
+  end
+
+  def set_layout
+    case action_name
+    when "splash"
+      "splash"
     end
 
-    def set_layout
-      case request.variant
-      when :tablet
-        "tablet"
-      when :desktop
-        "desktop"
-      else
-        "application"
-      end
-    end
+    # NOTE ccm:  I want to be able to switch between layouts using variants.
+    # Why do these not work?
+
+    # if browser.tablet?
+    #   "application"
+    # elsif !browser.mobile? && !browser.tablet?
+    #   "splash"
+    # else
+    #   "application"
+    # end
+
+    # case request.variant
+    # when browser.tablet?
+    #   "application"
+    # when !browser.mobile? && !browser.tablet?
+    #   "splash"
+    # else
+    #   "application"
+    # end
+
+    # if request.variant.tablet?
+    #   "application"
+    # elsif request.variant.desktop?
+    #   "splash"
+    # else
+    #   "application"
+    # end
+
+    # if request.variant.include? :tablet
+    #   "application"
+    # elsif request.variant.include? :desktop
+    #   "splash"
+    # else
+    #   "splash"
+    # end
+
+    # if params[:variant] == 'tablet'
+    #   "application"
+    # elsif params[:variant] == 'desktop'
+    #   "splash"
+    # else
+    #   "splash"
+    # end
+
+    # case request.variant
+    # when :tablet
+    #   "tablet"
+    # when :desktop
+    #   "splash"
+    # else
+    #   "splash"
+    # end
+  end
 end
