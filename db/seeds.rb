799e6acf31cdf71aa55636f9567f2cb9ee35df07
Chris Maxwell <cmaxwell@ojala.com>
2015-06-11 22:28:02 -0700

    Create preliminary category data, testing 2 different approaches to seating. Method 2 commented out.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = Category.create([
+  { name: 'Automotive Services' },
+  # { name: 'Maintenance' },
+  { name: 'Beauty' },
+  { name: 'Business' },
+  { name: 'Dating' },
+  { name: 'Dining' },
+  { name: 'Education and Training' },
+  { name: 'Farming / Livestock' },
+  { name: 'For Sale' },
+  # { name: 'Digital • Mobile • Electronic' },
+  # { name: 'Furniture • Appliances' },
+  # { name: 'Vehicle • Cycle • Marine' },
+  # { name: 'Department Store • General' },
+  # { name: 'Office Equipment' },
+  { name: 'Healthcare' },
+  { name: 'Top Industries' },
+  { name: 'Jobs' },
+  { name: 'Local Services' },
+  { name: 'Meet Up' },
+  { name: 'New Cars' },
+  { name: 'Part-time Jobs' },
+  { name: 'Pets / Pet Products' },
+  { name: 'Real Estate' },
+  { name: 'Recreation' },
+  { name: 'Resumes' },
+  { name: 'Ticketing' },
+  { name: 'Tourism' },
+  { name: 'Used Cars' }
+  # { name: 'By Brand' },
+  # { name: 'Manufacturer Certified Pre-Owned' }
+])
+
+# categories_data = YAML.load_file(Rails.root.join('db/seeds/categories.yml'))
+
+# categories_data.each do |category|
+#   category = Category.find_or_create_by(name, category['name'])
+#   game.save
+# end
