86bb9fe9e89bff6edd51acd10661b0cf58c94961
Chris Maxwell <cmaxwell@ojala.com>
2015-06-11 19:34:52 -0700

    Refactor routes.

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   get 'splash',   to: 'pages#splash',  as: :mock_splash
   get 'home',     to: 'pages#home',    as: :mock_home
 
-  get 'about',    to: 'pages#about',   as: :about
-  get 'terms',    to: 'pages#terms',   as: :terms
-  get 'privacy',  to: 'pages#privacy', as: :privacy
-  get 'contact',  to: 'pages#contact', as: :contact
+  get 'about',    to: 'pages#about'
+  get 'terms',    to: 'pages#terms'
+  get 'privacy',  to: 'pages#privacy'
+  get 'contact',  to: 'pages#contact'
 end
