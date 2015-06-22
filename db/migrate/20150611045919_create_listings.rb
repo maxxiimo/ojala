0510f0ea111ec8fa2d58c24bd841214678492725
Chris Maxwell <cmaxwell@ojala.com>
2015-06-22 11:07:27 -0700

    Change Listing attribute from name to title, refactor migrations into treat_listings.rb.

--- db/migrate/20150611045919_create_listings.rb
+++ db/migrate/20150611045919_create_listings.rb
@@ -1,12 +1,17 @@
 class CreateListings < ActiveRecord::Migration
   def change
     create_table :listings do |t|
-      t.string :title, null: false
-      t.text   :description
-      t.string :city
-      t.string :state
-      t.string :zipcode
-      t.string :country
+      t.string  :title, null: false
+      t.text    :description
+      t.string  :city
+      t.string  :state
+      t.string  :zipcode
+      t.string  :country
+      t.integer :category_id
+      t.integer :subcategory_id
+      # FIXME ccm: Should I be using the following? What's the difference?
+      # t.references :category_id, index: true, foreign_key: true
+      # t.references :subcategory_id, index: true, foreign_key: true
 
       t.timestamps null: false
     end
